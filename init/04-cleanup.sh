fc57dbe9a5d2e8a11ecfe40834aacd5cfe6dbd92
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2026-07-26 20:06:56 +0200

    Update and rename 02-cleanup.sh to 04-cleanup.sh

--- init/04-cleanup.sh
+++ init/04-cleanup.sh
@@ -10,11 +10,14 @@ rm -rf /var/lib/apt/lists/*
 rm -f /var/cache/apt/pkgcache.bin
 rm -f /var/cache/apt/srcpkgcache.bin
 
+find /var/cache/apt/archives -type f -name '*.deb' -print
 find /var/crash -xdev -mindepth 1 -delete 2>/dev/null
 find /var/lib/systemd/coredump -xdev -mindepth 1 -delete 2>/dev/null
-find /home/taw/.cache /root/.cache -xdev -mindepth 1 -delete 2>/dev/null
+
 find /tmp -xdev -mindepth 1 -delete
 find /var/tmp -xdev -mindepth 1 -delete
+
+find /home/taw/.cache /root/.cache -xdev -mindepth 1 -delete 2>/dev/null
 
 journalctl --rotate
 journalctl --vacuum-time=1s
