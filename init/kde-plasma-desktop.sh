fc08f8dbd9aa81a0f8bb83a485347be93054a45d
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2026-07-26 19:57:40 +0200

    Create kde-plasma-desktop.sh

---
+++ init/kde-plasma-desktop.sh
@@ -0,0 +1,16 @@
+apt-get install kde-plasma-desktop
+
+apt-get autoremove --purge \
+  kate \
+  kdeconnect \
+  konqueror \
+  plasma-discover \
+  kfind \
+  kwrite \
+  kde-spectacle \
+  zutty \
+  khelpcenter \
+  kinfocenter \
+  snapd \
+  plasma-welcome \
+  partitionmanager
